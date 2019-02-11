a974590184c215c98b20dee6421d461e4efb7d3c
Josh <Josh Kruse>
2019-02-11 11:07:27 -0700

    create replace spaces function

--- restclient.hs
+++ restclient.hs
@@ -4,7 +4,12 @@ import Network.HTTP
 
 api_key = "b03af69c"
 
-getMovieInfo :: String -> IO String
-getMovieInfo title = simpleHTTP req >>= getResponseBody 
+getInfo :: String -> IO String
+getInfo title = simpleHTTP req >>= getResponseBody 
     where req = getRequest url
           url = "http://www.omdbapi.com/?apikey=" ++ api_key ++ "&t=" ++ title
+
+replaceSpaces :: String -> String
+replaceSpaces ""         = ""
+replaceSpaces ( ' ':xs ) = '+': newLine xs
+replaceSpaces ( x:xs )   = x: newLine xs
